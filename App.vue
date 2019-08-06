d6f82997ea8b842a9ab6b45e0e76c41361d5013b
Carter <wjhsmarter@126.com>
2019-08-06 13:36:58 +0800

    提交更新检测

--- App.vue
+++ App.vue
@@ -10,17 +10,26 @@
 			...mapMutations(['login'])
 		},
 		onLaunch: function() {
-			let userInfo = uni.getStorageSync('userInfo') || '';
-			if(userInfo.id){
-				//更新登陆状态
-				uni.getStorage({
-					key: 'userInfo',
-					success: (res) => {
-						this.login(res.data);
-					}
-				});
-			}
-			
+		    const updateManager = uni.getUpdateManager();
+		    updateManager.onCheckForUpdate(function(res) {
+		        // 请求完新版本信息的回调
+		        console.log(res.hasUpdate);
+		    });
+		    updateManager.onUpdateReady(function(res) {
+		        uni.showModal({
+		            title: '更新提示',
+		            content: '发现新版本，是否重启应用？',
+		            success(res) {
+		                if (res.confirm) {
+		                    // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+		                    updateManager.applyUpdate();
+		                }
+		            }
+		        });
+		    });
+		    updateManager.onUpdateFailed(function(res) {
+		        // 新的版本下载失败
+		    });
 		},
 		onShow: function() {
 			console.log('App Show')
